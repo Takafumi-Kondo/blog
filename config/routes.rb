2bb8c8f7f6074b405ace93d3124c13b0b98ba6c8
takafumi-kondo <t.kily0121@gmail.com>
2019-06-10 04:29:18 +0000

    Add controllers views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+
+  root to: 'posts#index'
+  get '/about' => 'static_pages#about'
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :genres,         only: [:create, :index, :update, :destroy]
+  resources :emotions,       only: [:create, :index, :update, :destroy]
+  resources :contacts,       only: [:new, :create, :show, :index, :update, :destroy]
+  resources :relationships,  only: [:create, :destroy]
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
+  resources :posts do
+    resource :postimages
+    resource :favorites, only: [:create, :destroy]
+    resource :comments,  only: [:create, :destroy]
+  end
 end
