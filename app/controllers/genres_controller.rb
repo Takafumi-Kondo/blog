06a49bbdf4c85453d89f4a738546d507e364e679
takafumi-kondo <t.kily0121@gmail.com>
2019-06-11 10:50:13 +0000

    Update controller view of genre for register genres

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,13 +1,40 @@
 class GenresController < ApplicationController
+	#before_action :admin_user
+
   def create
+  	@genre = Genre.new(genre_params)
+  	if @genre.save
+  		redirect_to genres_path
+  	else
+  		render :index
+  	end
   end
 
   def index
+  	@genre = Genre.new
+  	@genres = Genre.page(params[:page]).per(10).reverse_order
   end
 
   def update
+  	genre = Genre.find(params[:id])
+  	if genre.update(genre_params)
+  		redirect_to genres_path
+  	else
+  		@genres = Genre.page(params[:page]).per(10).reverse_order
+  		render :index
+  	end
   end
 
   def destroy
+  	genre = Genre.find(params[:id])
+  	genre.destroy
+  	redirect_to genres_path
+  end
+
+
+  private
+
+  def genre_params
+  	params.require(:genre).permit(:genre_name)
   end
 end
