28d251239c7d8ef53f718e98977c8db2ce1d02e0
takafumi-kondo <t.kily0121@gmail.com>
2019-06-16 20:26:01 +0000

    Update views/controllers of post/header for search function

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,9 +12,9 @@ class Post < ApplicationRecord
 		favorites.where(user_id: user.id).exists?
 	end
 
-	def self.search(search)#selfはPosr
+	def self.search(search)#selfはPost
 		if search
-			where(['body LIKE ?', "%#{search}%"])#検索内とnameの部分一致を表示
+			where(['title LIKE ? OR body LIKE ?', "%#{search}%"])#検索内とnameの部分一致を表示
 		else# SQL記述 %は0字以上の任意文字列
 			all
 		end
