29d1fd5bb2c116c8433be09d18d2f3f55e07d2e2
takafumi-kondo <t.kily0121@gmail.com>
2019-06-18 09:17:27 +0000

    Update views to show user_headerimage

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '22398309698a8f607b30367380bd4bcf074b78cde2bc2226a02b87886dac3006d26e3a993ec6849b44fb6d327aa4c100699c5c6aae3c2a84224013874ae05395'
