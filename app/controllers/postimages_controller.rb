283d020e325ba5fcc5e1e1bf987fae7c3e47158b
takafumi-kondo <t.kily0121@gmail.com>
2019-06-27 22:12:08 +0000

    Fix all views for indent

--- app/controllers/postimages_controller.rb
+++ app/controllers/postimages_controller.rb
@@ -1,2 +1,20 @@
 class PostimagesController < ApplicationController
+# あとで複数画像投稿可予定
+
+  # before_action :authenticate_user!
+
+  # def create
+  #   post = Post.find(params[:post_id])
+  #   postimage = Postimage.new(postimage_params)
+  #   postimage.post_id = post.id
+  #   postimage.save
+  #   redirect_to post_path(post.id)
+  # end
+
+
+  # private
+
+  # def postimage_params
+  #   params.require(:postimage).permit(:post_id, :image)
+  # end
 end
